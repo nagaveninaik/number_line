8557be22a715c55802bf21b9b0995fed7d77a10b
nagaveninaik <70188382+nagaveninaik@users.noreply.github.com>
2021-09-19 11:56:34 +0530

    slider
    try to work slider property

--- src/game.js
+++ src/game.js
@@ -5,6 +5,9 @@ import boy from './assets/boy.png';
 import numberLine from './assets/numberLine.png';
 
 
+const COLOR_PRIMARY = 0x4e342e;
+const COLOR_LIGHT = 0x7b5e57;
+const COLOR_DARK = 0x260e04;
 class MyGame extends Phaser.Scene
 {
     constructor ()
@@ -12,93 +15,65 @@ class MyGame extends Phaser.Scene
         super();
     }
 
-//     preload ()
-//     {
-//         // this.load.scenePlugin({
-//         //     key: 'rexuiplugin',
-//         //     url: 'https://raw.githubusercontent.com/rexrainbow/phaser3-rex-notes/master/dist/rexuiplugin.min.js',
-//         //     sceneKey: 'rexUI'
-//         // });
+    preload ()
+    {
 
-//         this.load.image('numberLine',numberLine);
-//         this.load.image('boy',boy);
-//     }
+        this.load.image('numberLine',numberLine);
+        this.load.image('boy',boy);
+    }
    
        
-//     create ()
-//     {
+    create ()
+    {
         
-//         // var numberBar = this.rexUI.add.numberBar({
-//         //     x: 600,
-//         //     y: 800,
-//         //     width: 300, 
+        // var numberBar = this.rexUI.add.numberBar({
+        //     x: 600,
+        //     y: 800,
+        //     width: 300, 
 
-//         //     slider: {
-//         //         width: 100, // Fixed width
-//         //        track: this.rexUI.add.roundRectangle(0, 0, 0, 0, 10, COLOR_PRIMARY),
-//         //        indicator: this.rexUI.add.roundRectangle(0, 0, 0, 0, 20, COLOR_LIGHT),
-//         //        input: 'click',
-//         //    },
+        //     slider: {
+        //         width: 100, // Fixed width
+        //        track: this.rexUI.add.roundRectangle(0, 0, 0, 0, 10, COLOR_PRIMARY),
+        //        indicator: this.rexUI.add.roundRectangle(0, 0, 0, 0, 20, COLOR_LIGHT),
+        //        input: 'click',
+        //    },
 
-//     //        text: this.add.text(0, 0, '').setFixedSize(50, 0),
+    //        text: this.add.text(0, 0, '').setFixedSize(50, 0),
 
-//     //        space: {
-//     //            left: 10,
-//     //            right: 10,
-//     //            top: 10,
-//     //            bottom: 10,
+    //        space: {
+    //            left: 10,
+    //            right: 10,
+    //            top: 10,
+    //            bottom: 10,
 
-//     //            //icon: 10,
-//     //            slider: 10,
-//     //        },
+    //            //icon: 10,
+    //            slider: 10,
+    //        },
 
-//     //        valuechangeCallback: function (value, oldValue, numberBar) {
-//     //            numberBar.text = Math.round(Phaser.Math.Linear(-2, 2, value));
-//     //        },
-//     //    })
-//     //    .layout();
+    //        valuechangeCallback: function (value, oldValue, numberBar) {
+    //            numberBar.text = Math.round(Phaser.Math.Linear(-2, 2, value));
+    //        },
+    //    })
+    //    .layout();
 
-//         this.numberLine = this.add.image(375,850,'numberLine');
-//         this.numberLine.setOrigin(0.5);
-//         this.boy = this.add.image(375,700,'boy');
-//         this.x =2;
-//         this.y=3;
-//         this.z=this.x*this.y;
-//         this.values = [190,375,550];
-//         this.add.text(310, 350, `${this.x}*${this.y}= ${this.z}`, { fontFamily: 'serif',fontSize: '60px' });
-//         this.boy.setInteractive().on("pointerdown", (pointer, localX, localY, event)=>
-//         this.moveBoyTween(this.values[Phaser.Math.Between(0,2)])
-//         );
-        
-    
-// }
+        this.numberLine = this.add.image(375,850,'numberLine');
+        this.numberLine.setOrigin(0.5);
+        this.boy = this.add.image(375,700,'boy');
+        this.x =2;
+        this.y=3;
+        this.z=this.x*this.y;
+        this.values = [190,375,550];
+        this.add.text(310, 350, `${this.x}*${this.y}= ${this.z}`, { fontFamily: 'serif',fontSize: '60px' });
+        this.boy.setInteractive().on("pointerdown", (pointer, localX, localY, event)=>
+        this.moveBoyTween(this.values[Phaser.Math.Between(0,2)])
+        );
 
-// moveBoyTween(xVal)
-// {  
-//      this.tweens.add({
-//         targets: this.boy,
-//         x:xVal,
-//         duration: 1000,
-//         ease: "Power2"
-//     });
-// }
-
-
-preload() { 
-    this.load.scenePlugin({
-        key: 'rexuiplugin',
-        url: 'https://raw.githubusercontent.com/rexrainbow/phaser3-rex-notes/master/dist/rexuiplugin.min.js',
-        sceneKey: 'rexUI'
-    });      
-}
-
-create() {
-    var numberBar = this.rexUI.add.numberBar({
+        var numberBar = this.rexUI.add.numberBar({
             x: 400,
             y: 300,
             width: 300, // Fixed width
 
-            background: this.rexUI.add.roundRectangle(10, 10, 10, 10, 10, COLOR_DARK),
+            background: this.rexUI.add.roundRectangle(0, 0, 0, 0, 10, COLOR_DARK),
 
             icon: this.rexUI.add.roundRectangle(0, 0, 0, 0, 10, COLOR_LIGHT),
 
@@ -107,6 +82,7 @@ create() {
                 track: this.rexUI.add.roundRectangle(0, 0, 0, 0, 10, COLOR_PRIMARY),
                 indicator: this.rexUI.add.roundRectangle(0, 0, 0, 0, 10, COLOR_LIGHT),
                 input: 'click',
+                // gap: 0.1,
             },
 
             text: this.add.text(0, 0, '').setFixedSize(35, 0),
@@ -114,21 +90,83 @@ create() {
             space: {
                 left: 10,
                 right: 10,
-                top: 10,
-                bottom: 10,
+                top: 20,
+                bottom: 20,
 
                 icon: 10,
                 slider: 10,
             },
 
-            valuechangeCallback: function (value, oldValue, numberBar) {
-                numberBar.text = Math.round(Phaser.Math.Linear(0, 100, value));
+            valuechangeCallback: function (newValue, oldValue, numberBar) {
+                numberBar.text = Math.round(Phaser.Math.Linear(0, 100, newValue));
             },
-        })
-        .layout();
 
-    numberBar.setValue(75, 0, 100);
+            gap: 0.2,
+        })
+            .layout()
+            .drawBounds(this.add.graphics(), 0xff0000)
+
+        numberBar.setValue(75, 0, 100);
+        
+    
 }
+
+moveBoyTween(xVal)
+{  
+     this.tweens.add({
+        targets: this.boy,
+        x:xVal,
+        duration: 1000,
+        ease: "Power2"
+    });
+}
+
+
+// preload() { 
+//     this.load.scenePlugin({
+//         key: 'rexuiplugin',
+//         url: 'https://raw.githubusercontent.com/rexrainbow/phaser3-rex-notes/master/dist/rexuiplugin.min.js',
+//         sceneKey: 'rexUI'
+//     });      
+// }
+
+// create() {
+//     var numberBar = this.rexUI.add.numberBar({
+//             x: 400,
+//             y: 300,
+//             width: 300, // Fixed width
+
+//             background: this.rexUI.add.roundRectangle(10, 10, 10, 10, 10, COLOR_DARK),
+
+//             icon: this.rexUI.add.roundRectangle(0, 0, 0, 0, 10, COLOR_LIGHT),
+
+//             slider: {
+//                 // width: 120, // Fixed width
+//                 track: this.rexUI.add.roundRectangle(0, 0, 0, 0, 10, COLOR_PRIMARY),
+//                 indicator: this.rexUI.add.roundRectangle(0, 0, 0, 0, 10, COLOR_LIGHT),
+//                 input: 'click',
+//             },
+
+//             text: this.add.text(0, 0, '').setFixedSize(35, 0),
+
+//             space: {
+//                 left: 10,
+//                 right: 10,
+//                 top: 10,
+//                 bottom: 10,
+
+//                 icon: 10,
+//                 slider: 10,
+//             },
+
+//             valuechangeCallback: function (value, oldValue, numberBar) {
+//                 numberBar.text = Math.round(Phaser.Math.Linear(0, 100, value));
+//             },
+//         })
+//         .layout();
+
+//     numberBar.setValue(75, 0, 100);
+// }
     }
 
 
--- src/index.js
+++ src/index.js
@@ -1,16 +1,24 @@
 import Phaser from 'phaser';
 import MyGame from './game';
+import UIPlugin from '../node_modules/phaser3-rex-plugins/templates/ui/ui-plugin';
 
-const config = {
+var config = {
     type: Phaser.AUTO,
     parent: 'phaser-example',
     width: 750,
     height: 900,
     scale: {
         mode: Phaser.Scale.FIT,
-        autoCenter: Phaser.Scale.CENTER_BOTH
+        //autoCenter: Phaser.Scale.CENTER_BOTH
     },
-    scene: MyGame
+    scene: MyGame,
+    plugins: {
+        scene: [{
+            key: 'rexUI',
+            plugin: UIPlugin,
+            mapping: 'rexUI'
+        }]
+    }
 };
 
 const game = new Phaser.Game(config);
