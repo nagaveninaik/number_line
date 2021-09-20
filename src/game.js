2708fbd1770929c33799708defba838549d00c31
nagaveninaik <70188382+nagaveninaik@users.noreply.github.com>
2021-09-21 00:04:41 +0530

    try to work tween inside tween

--- src/game.js
+++ src/game.js
@@ -10,11 +10,17 @@ class MyGame extends Phaser.Scene
     constructor ()
     {
         super();
+        this.minLimit=-2;
+        this.maxLimit=2;
+        this.width=750;
+
+        this.x =0;
+        this.y=0;
+        this.z=0;
     }
 
     preload ()
     {
-
         this.load.image('numberLine',numberLine);
         this.load.image('boy',boy);
         this.load.image('addButton',addButton);
@@ -24,154 +30,125 @@ class MyGame extends Phaser.Scene
        
     create ()
     {
-        
         this.numberLine = this.add.image(375,850,'numberLine');
         this.numberLine.setOrigin(0.5);
         this.boy = this.add.image(375,700,'boy');
         
-         this.x =0;
-         this.xText=this.add.text(50,300,`${this.x}`,{ fontFamily: 'serif',fontSize: '80px' });
-         this.addButtonX1 = this.add.image(40,420,'addButton');
-         this.subButtonX1 = this.add.image(120,420,'subButton');
+        this.xText=this.add.text(50,300,`${this.x}`,{ fontFamily: 'serif',fontSize: '80px' });
+        this.addButtonX1 = this.add.image(40,420,'addButton');
+        this.subButtonX1 = this.add.image(120,420,'subButton');
 
-         this.addButtonX1.setInteractive().on("pointerdown",(pointer,localX,localY,event)=>
-         this.xValueIncrement());
+        this.addButtonX1.setInteractive().on("pointerdown",(pointer,localX,localY,event)=>
+        this.xValueIncrement());
 
-         this.subButtonX1.setInteractive().on("pointerdown",(pointer,localX,localY,event)=>
-         this.xValueDecrement());
+        this.subButtonX1.setInteractive().on("pointerdown",(pointer,localX,localY,event)=>
+        this.xValueDecrement());
 
-         this.add.text(200,300,`*`,{ fontFamily: 'serif',fontSize: '80px' });
+        this.add.text(200,300,`*`,{ fontFamily: 'serif',fontSize: '80px' });
 
-        this.y=0;
         this.yText=this.add.text(350,300,`${this.y}`,{ fontFamily: 'serif',fontSize: '80px' });
         this.addButtonY2 = this.add.image(340,420,'addButton');
         this.subButtonY2 = this.add.image(420,420,'subButton');
 
         this.addButtonY2.setInteractive().on("pointerdown",(pointer,localX,localY,event)=>
-         this.yValueIncrement());
+        this.yValueIncrement()
+        );
 
-         this.subButtonY2.setInteractive().on("pointerdown",(pointer,localX,localY,event)=>
-         this.yValueDecrement());
+        this.subButtonY2.setInteractive().on("pointerdown",(pointer,localX,localY,event)=>
+        this.yValueDecrement()
+        );
 
         this.add.text(500,300,`=`,{ fontFamily: 'serif',fontSize: '80px' });
 
-        this.z=0;
         this.zText = this.add.text(650,300,`${this.z}`,{ fontFamily: 'serif',fontSize: '80px' });
        
         this.clickBtn = this.add.image(680,420,'clickBtn');
         
-        this.values = [];
-        this.positionValue();
-        console.log(this.values)
-        // this.values.positionValue();
-        // console.log(this.values)
         this.clickBtn.setInteractive().on("pointerdown", (pointer, localX, localY, event)=>
             this.boyMovement()
-        
-        );
-           
-    }
-
-    positionValue()
-    {
-        this.width=750;
-        this.j=0;
-        this.i=30;
-        while(this.i<(this.width-50)){
-            if(this.j==0){
-                this.values[this.j]=this.i;
-                this.i+=30;
-                this.j+=1;
-            }
-            else{
-                this.i+=80;
-            this.values[this.j]=this.i;
-            this.j+=1;
-            }
-            
-        }
-            
-        
-        //(this.width/25),(this.width/11),(this.width/3.75),(this.width/2.632),(this.width/2),(this.width/1.624),(this.width/1.364),(this.width/1.182),(this.width/1.0417)
-    }
-
-    moveBoyTween(xVal)
-    { 
-    this.move= this.tweens.add({
-        targets: this.boy,
-        x:xVal,
-        duration: 3000,
-        ease: "Power2",
-        yoyo: true,
-    });
+        );  
     }
 
     xValueIncrement()
     {
-   if( this.x < 2){
-    this.x+=1;
-    this.xText.setText(`${this.x}`);
-    
-    }
+        if( this.x < this.maxLimit){
+            this.x+=1;
+            this.z=this.x*this.y;
+        }
     
     }
 
     xValueDecrement()
     {
-    if(this.x > -2){
-    this.x-=1;
-    this.xText.setText(`${this.x}`);
-   
-    }
+        if(this.x > this.minLimit){
+            this.x-=1;
+            this.z=this.x*this.y;
+         }
     }
 
     yValueIncrement()
     {
-    if( this.y < 2){
-    this.y+=1;
-    this.yText.setText(`${this.y}`);
-    
-    }
+        if( this.y < this.maxLimit){
+            this.y+=1;
+            this.z=this.x*this.y;
+        }
     }
 
     yValueDecrement()
     {
-    if(this.y > -2){
-    this.y-=1;
-    this.yText.setText(`${this.y}`);
-    
-    }
-    }
-    boyMovement(){
-    
-        
-        switch(this.z){
-            case 0 : this.moveBoyTween(this.values[4]);
-                break;
-            case 1 : this.moveBoyTween(this.values[5]);
-                break;
-            case 2 : this.moveBoyTween(this.values[6]);
-                break;
-            case 4 : this.moveBoyTween(this.values[8]);
-                break;
-            case -1 : this.moveBoyTween(this.values[3]);
-                break;
-            case -2 : this.moveBoyTween(this.values[2]);
-                break;
-            case -4 : this.moveBoyTween(this.values[0]);
-                break;
-
+        if(this.y > this.minLimit){
+            this.y-=1;
+            this.z=this.x*this.y;
         }
+    }
+
+    boyMovement(){
+        if(this.y==1||this.y==-1){
+        this.centerValue=(this.width/2);
+        this.position = (this.centerValue+(this.z*75));
+        console.log(this.position);
+        this.tweens.add({
+            targets: this.boy,
+            x:this.position,
+            duration: 1000,
+            ease: "Power2",
+            yoyo: true,
+        });
+    }
+     else if(this.y==2)
+        {
+            this.timeline = this.tweens.timeline({
+                targets: this.boy,
+                ease: 'Linear',       // 'Cubic', 'Elastic', 'Bounce', 'Back'
+                duration: 1000,
+                loop: 0,
+                
+            
+                tweens: [
+                    {
+                        targets: this.boy,
+                        x: (this.position/this.y),              
+                         ease: 'Linear',       
+                         duration: 1000,
+                         repeat: 0,            // -1: infinity
+                         yoyo: false,
+                         offset: '+=500',   // starts 500ms after previous tween ends
+                    },
+                    // ...
+                ]
+            });
+        }
+        
+       
 
     }
 
     update ()
     {
-        this.z=this.x*this.y;
+        this.xText.setText(`${this.x}`);
+        this.yText.setText(`${this.y}`);
         this.zText.setText(`${this.z}`);
     }
 }
-
-
 
 export default MyGame;
